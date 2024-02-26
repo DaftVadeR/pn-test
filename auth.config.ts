88e37bebcfe1ad8582c6983eb2b33575156f787a
Ross Davidson <tsuyoizuse@gmail.com>
2024-02-26 21:03:21 +0200

    chore: amend readme, remove console logs, add init script command.

--- auth.config.ts
+++ auth.config.ts
@@ -11,12 +11,10 @@ export const authConfig: NextAuthConfig = {
   debug: process.env.NODE_ENV !== "production",
   callbacks: {
     authorized: ({ auth, request: { nextUrl } }) => {
-      // console.log('authorized', auth, nextUrl);
       const isLoggedIn = !!auth?.user && !!auth?.user?.email;
       return isLoggedIn;
     },
     session: async ({ session, user, token }) => {
-      console.log('session', user, session, token);
       // session.user.id = user?.id;
       if(user) {
         session.jwt = user?.jwt;
@@ -30,16 +28,14 @@ export const authConfig: NextAuthConfig = {
       // session.user.jwt = token.sub!; // Adds user ID to session for use in server actions.
       return session;
     },
-    async signIn({ user, account, profile, email, credentials }) {
-      // console.log('signIn', user, account, profile );
+    async signIn({ user }) {
       if (user && user.email) return true;
       return false;
     },
-    async redirect({ url, baseUrl = process.env.NEXTAUTH_URL }) {
+    async redirect({ url }) {
       return url;
     },
-    async jwt({ token, user, account, profile }) {
-      console.log('jwt', user, token);
+    async jwt({ token, user }) {
       if(user) {
         token.jwt = user?.jwt;
         token.id = user?.id;
